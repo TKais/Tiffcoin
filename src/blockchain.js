e6ea2421e77a22b2a50b2ac4aec1a99d930651fd
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-03-07 20:52:16 -0800

    Create a method for adding a new block to the blockchain

--- src/blockchain.js
+++ src/blockchain.js
@@ -4,4 +4,14 @@ class Blockchain {
 	constructor() {
 		this.chain = [Block.createGenesisBlock()];
 	}
+
+	addBlock(data) {
+		const lastBlock = this.chain[this.chain.length - 1];
+		const block = Block.createMineBlock(lastBlock, data);
+
+		this.chain.push(block);
+		return block;
+	}
 }
+
+module.exports = Blockchain;
