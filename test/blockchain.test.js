b656e7afe774b04c804e591837ffbc76ab50f6ca
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-03-12 19:25:57 -0700

    Add test for validating a blockchain

--- test/blockchain.test.js
+++ test/blockchain.test.js
@@ -2,19 +2,27 @@ const Blockchain = require('../src/blockchain');
 const Block = require('../src/block');
 
 describe('Blockchain', () => {
-	let newBlockchain;
+	let firstBlockchain;
+	let secondBlockchain;
 
 	beforeEach(() => {
-		newBlockchain = new Blockchain();
+		firstBlockchain = new Blockchain();
+		secondBlockchain = new Blockchain();
 	});
 
 	it('starts with genesis block', () => {
-		expect(newBlockchain.chain[0].hash).toEqual('f1r57-h45h');
+		expect(firstBlockchain.chain[0].hash).toEqual('f1r57-h45h');
 	});
 
 	it('adds a new block', () => {
 		const data = 'example';
-		newBlockchain.addBlock(data);
-		expect(newBlockchain.chain[newBlockchain.chain.length - 1].data).toEqual(data);
+		firstBlockchain.addBlock(data);
+		expect(firstBlockchain.chain[firstBlockchain.chain.length - 1].data).toEqual(data);
+	});
+
+	it('validates a valid chain', () => {
+		secondBlockchain.addBlock('foo');
+
+		expect(firstBlockchain.chainIsValid(secondBlockchain.chain)).toBe(true);
 	});
 })
