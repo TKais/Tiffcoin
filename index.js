0917be33ab8fe0c19269adeb0c90ab8c57e099c3
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-03-24 11:05:30 -0700

    Add a post request for creating a new block

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const Blockchain = require('./src/blockchain');
+const bodyParser = require('body-parser');
 const PORT = process.env.port || 8000;
 
 const app = express();
 const chainInstance = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/blocks', (request, response) => {
 	response.json(chainInstance.chain);
+});
+
+app.post('/mine', (request, response) => {
+	const block = chainInstance.addBlock(request.body.data);
+	response.redirect('/blocks');
 });
 
 app.listen(PORT, () => {
