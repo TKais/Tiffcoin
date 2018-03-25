0512ccc69557229040140345473a6bfa3cb04526
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2018-03-25 15:15:34 -0700

    Add P2PServer class for creating websockets

---
+++ p2pServer.js
@@ -0,0 +1,12 @@
+const Websocket = require('ws');
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+
+class P2PServer {
+	constructor(blockchain) {
+		this.blockchain = blockchain;
+		this.sockets = [];
+	}
+}
